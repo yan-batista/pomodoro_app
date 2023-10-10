f8fa2f74397dcddaae9b55bbd8e9f6e58501d830
Yan  Batista <y.batista@gsuite.iff.edu.br>
2023-10-10 16:55:07 -0300

    update screenshots

--- src/constants.ts
+++
@@ -1,9 +0,0 @@
-const colorVariants = {
-  red: "bg-accent_red",
-  cyan: "bg-accent_cyan",
-  purple: "bg-accent_purple",
-  blue: "bg-blue",
-  input_color: "bg-input_color",
-};
-
-export default colorVariants;
